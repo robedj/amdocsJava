95e3082123feb277ad8a6e66b18efcb908942607
robedj <ed.wassouf@gmail.com>
2021-12-16 16:05:40 -0400

    desafio

---
+++ src/ViraTempo.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ViraTempo {
+
+public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        while (leitor.hasNext()) {
+        int graus = leitor.nextInt();
+        if (                                )
+        System.out.println("Bom Dia!!");
+
+
+
+
+        else System.out.println("Bom Dia!!");
+        }
+        }
+
+        }
