59b98a00ba7f70e0a0a1bae7584da0bde21a4e30
robedj <ed.wassouf@gmail.com>
2021-12-16 16:32:49 -0400

    resolvido

--- src/ViraTempo.java
+++ src/ViraTempo.java
@@ -3,18 +3,25 @@ import java.util.Scanner;
 
 public class ViraTempo {
 
-public static void main(String[] args) throws IOException {
-        Scanner leitor = new Scanner(System.in);
-        while (leitor.hasNext()) {
-        int graus = leitor.nextInt();
-        if (                                )
-        System.out.println("Bom Dia!!");
+        public static void main(String[] args) throws IOException {
+                Scanner leitor = new Scanner(System.in);
+                String mensagem;
+                while (leitor.hasNext()) {
+                        int graus = leitor.nextInt();
+                        if ((graus == 360) || (graus < 90)) {
+                                mensagem = "Bom Dia!!";
+                        } else if ((graus >= 90) && (graus < 180)) {
+                                mensagem = "Boa Tarde!!";
+                        } else if ((graus >= 180) && (graus < 270)) {
+                                mensagem = "Boa Noite!!";
+                        }else mensagem = "De Madrugada!!";
 
+                        System.out.println(mensagem);
 
-
-
-        else System.out.println("Bom Dia!!");
+                }
+                leitor.close();
         }
-        }
+}
 
-        }
+
+
