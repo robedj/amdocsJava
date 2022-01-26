8e2b3732445b68b825bc4f518940f67ab62da9f9
robedj <ed.wassouf@gmail.com>
2022-01-26 12:23:41 -0400

    Fatorial Recursivo ok

---
+++ src/exercicios/FatorialRecursivo.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class FatorialRecursivo {
+
+    public static int fatorial(int x) {
+        if (x <= 1) {
+            return 1;
+        } else {
+            return x * fatorial(x - 1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int numero;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Fatorial: ");
+
+        numero = entrada.nextInt();
+
+        System.out.println("Fatorial de: " + numero+ " = " + fatorial(numero));
+
+    }
+}
+
+
