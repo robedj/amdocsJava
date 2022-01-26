db13242c5a6e19f98f1b9d6d7947c3256ed31d67
robedj <ed.wassouf@gmail.com>
2022-01-26 09:58:07 -0400

    exec1

---
+++ src/exercicios/Exec1NomeIdade.java
@@ -0,0 +1,23 @@
+package exercicios;
+import java.util.Scanner;
+
+
+
+public class Exec1NomeIdade {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+        while (true) {
+            System.out.println("Nome: ");
+            nome = scan.next();
+            if(nome.equals("0"))break;
+            System.out.println("Idade: ");
+            idade = scan.nextInt();
+        }
+        System.out.println("Continua aqui..");
+    }
+}
+
+   
