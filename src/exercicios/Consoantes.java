2fc43895aec228c35b498dc374b1ecdf6bfbd123
robedj <ed.wassouf@gmail.com>
2022-01-26 14:08:52 -0400

    Consoantes Ok

---
+++ src/exercicios/Consoantes.java
@@ -0,0 +1,35 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Consoantes {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String[] consoantes = new String[6];
+        int cont = 0;
+        int qtdeConsoantes= 0;
+
+        do {
+            System.out.println("Letra: ");
+            String letra = scan.next();
+
+            if(letra.equalsIgnoreCase("a")|
+                letra.equalsIgnoreCase("e")|
+                letra.equalsIgnoreCase("i")|
+                letra.equalsIgnoreCase("o")|
+                letra.equalsIgnoreCase("u")){
+                consoantes[cont]=letra;
+                qtdeConsoantes++;
+            }
+                cont++;
+
+        }while(cont < consoantes.length);
+        System.out.println("Consoantes: ");
+        for(String consoante: consoantes){
+            if(consoante!= null) {
+                System.out.println(consoante + " ");
+            }
+        }
+    }
+}
